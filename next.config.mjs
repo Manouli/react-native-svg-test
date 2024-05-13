212ced19f363e12b3c3b4e0c1a5420d9c35f46f1
Emmanouil Papoutsakis <m_papous@hotmail.com>
2024-05-13 16:34:08 +0100

    initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      'react-native$': 'react-native-web',
+    };
+
+    config.resolve.extensions = [
+      '.web.js',
+      '.web.ts',
+      '.web.tsx',
+      ...config.resolve.extensions
+    ];
+
+    if (options.isServer) {
+      config.externals = ['react', 'react-native-web', ...config.externals];
+    }
+
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false // the solution
+    };
+
+    return config;
+  }
 };
 
 export default nextConfig;
